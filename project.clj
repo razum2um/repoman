c30219bafa83bdf97bb04a1beeb9d97030e27c6a
Vlad Bokov <razum2um@mail.ru>
2014-05-31 22:01:58 +0700

    get-stats & fetch-stats

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "https://github.com/razum2um/repoman"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cheshire "5.3.1"]
+                 [korma "0.3.1"]
+                 [environ "0.5.0"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [clj-http "0.9.2"]]
   :main ^:skip-aot repoman.core
   :target-path "target/%s"
   :profiles {
--- src/repoman/core.clj
+++ src/repoman/core.clj
@@ -1,12 +1,59 @@
 (ns repoman.core
+  (:require [clojure.pprint :refer :all])
+  (:require [environ.core :refer [env]])
+  (:require [korma.db :refer :all])
+  (:require [korma.core :refer :all])
+  (:require [clj-http.client :as client])
+  (:require [cheshire.core :as json])
   (:gen-class))
 
-(defn first-element [sequence default]
-  (if (empty? sequence)
-    default
-    (first sequence)))
+(def repo (env :repo))
+(def username (env :username))
+(def cookie (env :cookie))
+(def db-creds (env :database))
+
+(defdb db (postgres db-creds))
+
+(defentity users)
+(defentity repos
+  (belongs-to users {:fk 'user_id}))
+(defentity stats
+  (belongs-to repos {:fk 'repo_id}))
+
+(class (get (first (select users)) :updated_at))
+
+(defn get-stats [user repo timestamp]
+  (select stats
+    (fields :stats.updated_at :total :unique)
+    (with repos (fields) (where {:name repo})
+          (with users (fields) (where {:name user})))))
+
+;; (-> (select* stats)
+;;     ;;(fields)
+;;     (with repos (fields)
+;;           (with users (fields))) (as-sql))
+
+
+
+(defn fetch-stats [user repo cookie]
+  (client/get
+   (format "https://github.com/%s/%s/graphs/traffic-data" user repo)
+   {:cookies { "user_session" { :value cookie }}}
+   :as :json))
+
+(defn get-counts [user repo cookie]
+  (json/parse-string
+   (get
+    (fetch-stats user repo cookie)
+    :body)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (pprint (get-counts username repo cookie))
+  (pprint (get-stats username repo cookie)))
+
+
+;;(get-counts username repo cookie)
+;;(get-stats username repo cookie)
